7fdd9efbc761e827af182c189cd5461472f96b55
nicholas-karimi <kariminic@gmail.com>
2020-09-23 17:56:07 +0300

    transform web into components and use props to display messages

--- main.js
+++ main.js
@@ -1,42 +1,97 @@
+// register component
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    
+    <div class="product">
+    <div class="product-image">
+        <img v-bind:src="image">
+    </div>
+    <div class="product-info">
+        <!-- <h1>Product goes here!</h1> -->
+        <!-- <h1>{{ brand }} {{ product }}</h1> -->
+        <h1>{{ title }}</h1>
+        <p v-show="inStock">In Stock</p>
+        <p>{{sale}}</p>
+        <p>Shipping: {{shipping}}</p>
+        <!-- <p v-if="inventory <= 10 && inventory > 0">In Stock</p>
+        <p v-else="inventory">Out of Stock</p> -->
 
-//vue instance
-var app = new Vue({
-    el: '#app',
-    data: {
-    	// attribute binding
-        brand: 'Nski',
-        product: 'Socks',
-        selectedVariant: 0,
-        url: 'https://www.google.com/',
+        <!-- <li><a :href="url">More</a></li>  use v-bind shorthand :-->
 
-        //conditional
-        // inventory: 100,
-        // inStock: false, - replaced with computed property
-        // list rendering
-        details: ['80% cotton', '20 % polyester', 'Gender-neutral'],
+        <!-- list rendering -->
+        <ul>
+            <li v-for="detail in details" >{{ detail }}</li>
+        </ul>
+        <div class="color-box" v-for="(variant, index) in variants"
+        :key="variant.variantId"
+        :style="{backgroundColor: variant.variantColor}"
+        @mouseover="updateProduct(index)">
+            <!-- <p @mouseover="updateProduct(variant.variantImage)">{{ variant.variantColor }}</p> -->
 
-        variants: [
-            {
-                variantId: 2345,
-                variantColor: 'green',
-                variantImage: './assets/green.jpeg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2018,
-                variantColor: 'blue',
-                variantImage: './assets/fancy.jpeg',
-                variantQuantity: 0
+        </div>
+        <div class="">
+            <h3>Available Sizes:</h3>
+            <ol>
+                <li v-for="size in sizes">{{ size }}</li>
+            </ol>
+        </div>
+        <button type="button" v-on:click="addToCart"
+                            :disabled="!inStock"
+                            :class="{disabledButton: !inStock}">Add to Cart</button>
+        <button type="button" v-on:click="removeItem">Remove Item</button>
+        <div class="cart">
+            <p>Cart {{cart}}</p>
+        </div>
+       
+    </div>
 
-            }
-        ],
+</div>
 
-        sizes: ['M', 'L', 'XL', 'XXL', 'S'],
+    `,
 
-        cart: 0,
-        onSale: true
-
-
+    data(){
+        return {
+            // attribute binding
+            brand: 'Nski',
+            product: 'Socks',
+            selectedVariant: 0,
+            url: 'https://www.google.com/',
+    
+            //conditional
+            // inventory: 100,
+            // inStock: false, - replaced with computed property
+            // list rendering
+            details: ['80% cotton', '20 % polyester', 'Gender-neutral'],
+    
+            variants: [
+                {
+                    variantId: 2345,
+                    variantColor: 'green',
+                    variantImage: './assets/green.jpeg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2018,
+                    variantColor: 'blue',
+                    variantImage: './assets/fancy.jpeg',
+                    variantQuantity: 0
+    
+                }
+            ],
+    
+            sizes: ['M', 'L', 'XL', 'XXL', 'S'],
+    
+            cart: 0,
+            onSale: true
+    
+    
+        }
     },
     // instance methods
     methods: {
@@ -69,6 +124,21 @@ var app = new Vue({
                 return ` Our ${this.brand} ${this.product} is on SALE`;
 
             }
+        },
+        shipping(){
+            if(this.premium){
+                return "Free";
+            }else{
+                return '$2.99';
+            }
         }
     }
+})
+//vue instance
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false
+    }
+    
 })
